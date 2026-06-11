09f8e53eadf5a46077cd07ece420a5490cec5881
kevenlwr <keven.santana@nave.org.br>
2026-06-11 11:07:33 -0300

    Atv parcial

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -8,3 +8,18 @@
   * "indexAtividade.js" de dentro da pasta src:
   */
 
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+const cors = require('cors');
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get("/index",(req,res) =>{
+  res.sendFile(path.join(__dirname, 'indexAtividade.html'));
+})
+
+app.listen(port, () => {
+    console.log('Servidor rodando! Acesse http://localhost:3000');
+});
--- src/AtvEnviarJson.js
+++ src/AtvEnviarJson.js
@@ -13,3 +13,19 @@
   * }
   */
 
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json());
+  
+app.get("/objeto", (req,res) => {
+  res.json({
+    acesso: "OK",
+   titulo: "Responda a pergunta abaixo:",
+  pergunta: "Quem é mais redondo, o Kirby ou o Jigglypuff?",
+  })
+})
+
+app.listen(3000, () => {
+    console.log('bolinha de golfe Acesse http://localhost:3000');
+});
