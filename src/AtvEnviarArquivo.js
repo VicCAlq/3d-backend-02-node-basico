261a4f18bbd5c572fbfa042ae27e9cf603cdaadc
Cassianjo <cassiano.jorge@nave.org.br>
2026-06-18 10:14:48 -0300

    syhm

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -8,3 +8,19 @@
   * "indexAtividade.js" de dentro da pasta src:
   */
 
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 3000
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Envio de um arquivo html separado
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src' ,'indexAtividade.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
