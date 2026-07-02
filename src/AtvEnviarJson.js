0e56e240a145affeb2a2098781766121e9e6565e
PedroHenrique2D <pedrohenrique.barbosa@nave.org.br>
2026-07-02 14:12:45 -0300

    Replace /inicio route with /objeto returning JSON

--- src/AtvEnviarJson.js
+++ src/AtvEnviarJson.js
@@ -15,18 +15,12 @@
 
 const express = require("express");
 const app = express();
-app.get("/inicio", (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Exemplo de página</title>
-      </head>
-      <body>
-        <h1>Você acessou a página de início!</h1>
-        <p>Passe um PIX de 1 real como pedágio :D</p>
-      </body>
-    </html>
-  `);
+app.get("/objeto", (req, res) => {
+  res.json({
+    acesso: "OK",
+    titulo: "Responda a pergunta abaixo:",
+    pergunta: "Quem é mais redondo, o Kirby ou o Jigglypuff?"
+  });
 });
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
