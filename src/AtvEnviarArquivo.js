12633985bb81660cbff38828af312023e2fefea4
Nini2pi <mariafernanda.duarte@nave.org.br>
2026-07-02 14:02:32 -0300

    Add express route to serve indexAtividade.html

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -7,4 +7,12 @@
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.js" de dentro da pasta src:
   */
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'indexAtividade.html'));
+});
 
