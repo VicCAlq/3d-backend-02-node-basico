6f646aa92cca64a6af592e55f08c708a11ffa05b
PedroHenrique2D <pedrohenrique.barbosa@nave.org.br>
2026-07-02 14:04:32 -0300

    Update AtvEnviarArquivo.js

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -7,12 +7,12 @@
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.html" de dentro da pasta src:
   */
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'indexAtividade.html'));
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "indexAtividade.html"));
 });
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+  console.log("Servidor rodando na porta 3000");
 });
