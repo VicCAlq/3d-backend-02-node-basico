43126eea02af278a3a8a9145965b3fc929859ade
PedroHenrique2D <pedrohenrique.barbosa@nave.org.br>
2026-07-02 14:08:14 -0300

    Update AtvEnviarHTML.js

--- src/AtvEnviarHTML.js
+++ src/AtvEnviarHTML.js
@@ -17,10 +17,19 @@
   *  </html>
   */
 const express = require("express");
-const path = require("path");
 const app = express();
-app.get("/index", (req, res) => {
-  res.sendFile(path.join(__dirname, "indexAtividade.html"));
+app.get("/inicio", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Exemplo de página</title>
+      </head>
+      <body>
+        <h1>Você acessou a página de início!</h1>
+        <p>Passe um PIX de 1 real como pedágio :D</p>
+      </body>
+    </html>
+  `);
 });
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
