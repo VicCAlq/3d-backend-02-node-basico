eb239a1635df6a7ecf17d2843b3213c0b3ba8bfc
PedroHenrique2D <pedrohenrique.barbosa@nave.org.br>
2026-07-02 14:15:23 -0300

    Update AtvTresRotas.js

--- src/AtvTresRotas.js
+++ src/AtvTresRotas.js
@@ -28,4 +28,32 @@
   * }
   */
 
+const express = require("express");
+const path = require("path");
+const app = express();
+app.get("/uno", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Tudo começa aqui</title>
+      </head>
+      <body>
+        <h1>A próxima rota é em Inglês</h1>
+        <p>Para acessar ela, como se chama o número 2 em inglês?</p>
+      </body>
+    </html>
+  `);
+});
+app.get("/two", (req, res) => {
+  res.sendFile(path.join(__dirname, "two.html"));
+});
+app.get("/san", (req, res) => {
+  res.json({
+    titulo: "San",
+    conteudo: "Próximas atividades a gente elabora os outros métodos HTML"
+  });
+});
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
 
