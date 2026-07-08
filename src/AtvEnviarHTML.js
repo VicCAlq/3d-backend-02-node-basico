f9bc634db1a7b3bd442b4f11670b1913b2fe45b5
mavimdddu <mariavitoria.matos@nave.org.br>
2026-07-08 10:52:37 -0300

    Update AtvEnviarHTML.js

--- src/AtvEnviarHTML.js
+++ src/AtvEnviarHTML.js
@@ -17,29 +17,24 @@
   *  </html>
   */
 
+const express = require('express')
+const app = express()
+const port = 3000
 
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const app = express();
-
-
-
-
-// Envio de HTML escrito direto no código
-app.get('/inicio', (req, res) => {
-  res.send(`
-    <html>
+app.get("/inicio", (req, res) => {
+    res.send(`
+       <html>
       <head>
-       <title>Exemplo de página</title>
-     </head>
-  <body>
-      <h1>Você acessou a página de início!</h1>
+        <title>Exemplo de página</title>
+      </head>
+      <body>
+        <h1>Você acessou a página de início!</h1>
         <p>Passe um PIX de 1 real como pedágio :D</p>
       </body>
-    </html>
+   </html>
     `);
 });
+
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+    console.log('Servidor rodando! Acesse http://localhost:3000');
 });
