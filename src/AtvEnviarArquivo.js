7d07f0b42d9df7b7814948227714bfa9eae91581
harieladorecife <mariaantonia.lima@nave.org.br>
2026-07-08 10:50:30 -0300

    Update AtvEnviarArquivo.js

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -6,7 +6,31 @@
   *
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.js" de dentro da pasta src:
+  *//*
+  * [ATIVIDADE 2 - Enviar arquivo HTML]
+  *
+  * Crie um aplicativo Express com uma única rota do tipo "GET".
+  * O endereço desta rota deve ser "/index".
+  *
+  * Ao acessar esta rota, deve ser enviado o arquivo 
+  * "indexAtividade.js" de dentro da pasta src:
   */
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+const cors = require('cors');
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get("/index",(req,res) =>{
+  res.sendFile(path.join(__dirname, 'indexAtividade.html'));
+})
+
+app.listen(port, () => {
+    console.log('Servidor rodando! Acesse http://localhost:3000');
+});
 /*
   * [ATIVIDADE 2 - Enviar arquivo HTML]
   *
