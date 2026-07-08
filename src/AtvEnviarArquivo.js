b3e9d5709bb97224aa88b300a21a9ee89b2d8eb8
harieladorecife <mariaantonia.lima@nave.org.br>
2026-07-08 10:45:03 -0300

    Update AtvEnviarArquivo.js

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -7,12 +7,25 @@
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.js" de dentro da pasta src:
   */
+// Importa o Express
 const express = require('express');
+
+// Importa algumas funcionalidades que veremos depois
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
 
+// Permite que o servidor entenda JSON no corpo das requisições
+app.use(express.json());
+// Habilita CORS (veremos sobre isso depois)
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'indexAtividade.html'));
+  res.sendFile(path.join(__dirname, 'indexAtividade.html'));
 });
 
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
