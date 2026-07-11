b2ec3b88d7745d2ab5aa4b1702201e1a53026ba5
SenaPaiva <mariaeduarda.sena@nave.org.br>
2026-07-11 19:25:28 -0300

    Update AtvTresRotas.js

--- src/AtvTresRotas.js
+++ src/AtvTresRotas.js
@@ -27,5 +27,38 @@
   *   conteudo: "Próximas atividades a gente elabora os outros métodos HTML"
   * }
   */
+const express = require('express');
 
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/uno', (req, res) => {
+  res.send(`
+  <html>
+  <head>
+  <title>Tudo começa aqui</title>
+  </head>
+  <body>
+  <h1>A próxima rota é em Inglês</h1>
+  <p>Para acessar ela, como se chama o número 2 em inglês?</p>
+  </body>
+  </html>
+    `);
+});
+ app.get('/two', (req, res) => {
+  res.sendFile(path.join(__dirname, 'two.html'));
+});
+
+app.get('/san', (req, res) => {
+  res.json({
+    titulo: "San",
+    conteudo: "Próximas atividades a gente elabora os outros métodos HTML"
+  });
+});
 
